b088b13d6e05e7f295b527555b269be10a092a3e
Kosa <116841035+Idiakosa@users.noreply.github.com>
2025-06-15 13:40:08 -0400

    Add files via upload

---
+++ originalArt.java
@@ -0,0 +1,17 @@
+package originalAsciiArt; 
+
+public class originalArt {
+	public static void main(String[] args) {
+	System.out.println(" (\\   /) ");
+	System.out.println(" (o\\~/o) ");
+	System.out.println("  { ' '} ");
+	System.out.println(" ( -- -- ) ");
+	System.out.println(" ((\\- -/)) ");
+	System.out.println("  / | | \\ ");
+	System.out.println(" *  *  *  ");
+	System.out.println(" Hop hop! ");
+/* original ascii art piece (bunny) using 8 print statements
+ * 8 characters wide and at least 20 chars that
+ * aren't space*/
+	}
+}
---
+++ simpleArt.java
@@ -0,0 +1,15 @@
+package simpleAsciiArt; 
+
+public class simpleArt {
+	public static void main(String[] args) {
+	System.out.println("   ___   ");
+	System.out.println("  _ _ _  ");
+	System.out.println(" _     _ ");
+	System.out.println("_  _ _  _");
+	System.out.println("_   _   _");
+	System.out.println(" _     _ ");
+	System.out.println("  _ _ _  ");
+	System.out.println("   ___   ");
+/* wonky smiley face made with underscores "_" */
+	}
+}
